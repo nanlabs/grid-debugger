f03879a8c845fa541b3a9d44bbae9a4c001a45ba
matiasalvarez87 <matiasalvarez87@gmail.com>
2015-02-25 10:13:10 -0300

    Build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,28 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.initConfig({
     connect: {
       uses_defaults: {}
+    },
+    uglify: {
+      build: {
+        files: {
+          'dist/grin-debugger.min.js': 'src/grid-debugger.js'
+        }
+      }
+    },
+    copy: {
+      build: {
+        files: [
+          { src: 'src/*', dest: 'dist/' },
+          { src: 'bower.json', dest: 'dist/' },
+          { src: 'index.html', dest: 'dist/' }
+        ]
+      }
     }
   });
   grunt.registerTask('default', ['connect']);
+  grunt.registerTask('build', ['copy', 'uglify']);
 };
---
+++ dist/src/grid-debugger.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  function createEl(type, id, className) {
+    var e = document.createElement(type);
+    if (id)
+      e.id = id;
+    if (className)
+      e.className = className;
+    return e;
+  }
+
+  function appendEl(e, type, id, className) {
+    var newEl = createEl(type, id, className);
+    e.appendChild(newEl);
+    return newEl;
+  }
+
+  function createStylesheet() {
+    var style = document.createElement("style");
+    style.appendChild(document.createTextNode(""));
+    document.head.appendChild(style);
+    return style.sheet;
+  }
+
+  function addCSSRule(sheet, selector, rules, index) {
+    if("insertRule" in sheet) {
+      sheet.insertRule(selector + "{" + rules + "}", index);
+    }
+    else if("addRule" in sheet) {
+      sheet.addRule(selector, rules, index);
+    }
+  }
+
+
+  // Setup functions
+
+  function setupGridDebuggerStyles() {
+    var sheet = createStylesheet();
+    addCSSRule(sheet, "#grid-container" , "position: fixed; top: 0; left: 0; height: 100%; width: 100%; background-color: rgba(201, 201, 201, .5);");
+    addCSSRule(sheet, "#grid-debugger .container, #grid-debugger .container-fluid, #grid-debugger .row", "height: 100%; opacity: .5;");
+    addCSSRule(sheet, "#grid-debugger .row > div", "opacity: .7; height: 100%; border: 1px solid yellow; background-image:linear-gradient(to bottom, rgba(240, 255, 40, 1) 0%, rgba(240, 255, 40, 1) 100%), linear-gradient(to bottom, rgba(240, 40, 40, 1) 0%, rgba(240, 40, 40, 1) 100%); background-clip: content-box, padding-box;");
+    addCSSRule(sheet, "#grid-debugger .btn-group", "position: fixed; top: 0");
+  }
+
+  function setupGrid() {
+    var grid = createEl('div', 'grid-debugger'),
+        gridContainer = appendEl(grid, 'div', 'grid-container'),
+        container = appendEl(gridContainer, 'div', null, 'container'),
+        row = appendEl(container, 'div', null, 'row');
+
+    for (var i = 0; i < 12; i++) {
+      appendEl(row, 'div', null, 'col-xs-1');
+    }
+    document.body.insertBefore(grid, document.body.childNodes[0]);
+  }
+
+  function setupButtons() {
+    var grid = document.getElementById('grid-debugger'),
+        btnGroup = appendEl(grid, 'div', null, 'btn-group'),
+        toggleBtn = appendEl(btnGroup, 'button', null, 'btn btn-default glyphicon glyphicon-th'),
+        fluidBtn = appendEl(btnGroup, 'button', null, 'btn btn-default glyphicon glyphicon-resize-full'),
+        fixedBtn = appendEl(btnGroup, 'button', null, 'btn btn-default glyphicon glyphicon-resize-small');
+    toggleBtn.onclick = toggleShow;
+    fluidBtn.onclick = changeToFluidGrid;
+    fixedBtn.onclick = changeToFixedGrid;
+  }
+
+  function _changeTo(from, to) {
+    var grid = document.getElementById('grid-debugger'),
+        container = grid.getElementsByClassName(from)[0];
+    if (container)
+      container.className = to;
+  }
+
+
+  // Public functions
+
+  function toggleShow() {
+    var grid = document.getElementById('grid-container');
+    if(grid.style.display != "none")
+      grid.style.display = "none";
+    else
+      grid.style.display = "block";
+  }
+
+  function changeToFixedGrid() {
+    _changeTo('container-fluid', 'container');
+  }
+
+  function changeToFluidGrid() {
+    _changeTo('container', 'container-fluid');
+  }
+
+  window.gridDebugger = {
+    init: function () {
+      setupGridDebuggerStyles();
+      setupGrid();
+      setupButtons();
+      toggleShow();
+    },
+    toggleShow: toggleShow,
+    changeToFluidGrid: changeToFluidGrid,
+    changeToFixedGrid: changeToFixedGrid
+  };
+});
